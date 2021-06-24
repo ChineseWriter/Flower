dba32aea7281fc3cdfde5655a39bd61667d8d8bd
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2021-06-24 19:42:39 +0800

    初始化项目。

---
+++ Flower/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :__init__.py.py
+# @Time      :2021/6/24 19:41
+# @Author    :Amundsen Severus Rubeus Bjaaland
+
+
+if __name__ == "__main__":
+    run_code = 0
